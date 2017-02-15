ce028c0de5953e1b99c288f8da4f957e7c527ced
Pandian <pandian4mail@gmail.com>
2017-02-15 07:58:58 -0600

    Fixing indentation

--- src/main/java/nlp/lm/BigramModel.java
+++ src/main/java/nlp/lm/BigramModel.java
@@ -3,16 +3,16 @@ package nlp.lm;
 import java.io.*;
 import java.util.*;
 
-/** 
+/**
  * @author Ray Mooney
  * A simple bigram language model that uses simple fixed-weight interpolation
  * with a unigram model for smoothing.
-*/
+ */
 
 public class BigramModel {
 
     /** Unigram model that maps a token to its unigram probability */
-    public Map<String, DoubleValue> unigramMap = null; 
+    public Map<String, DoubleValue> unigramMap = null;
 
     /**  Bigram model that maps a bigram as a string "A\nB" to the
      *   P(B | A) */
@@ -31,132 +31,130 @@ public class BigramModel {
      *  unigram entries for setence start (<S>), sentence end (</S>)
      *  and unknown tokens */
     public BigramModel() {
-		unigramMap = new HashMap<String, DoubleValue>();
-		bigramMap = new HashMap<String, DoubleValue>();
-		unigramMap.put("<S>", new DoubleValue());
-		unigramMap.put("</S>", new DoubleValue());
-		unigramMap.put("<UNK>", new DoubleValue());
+        unigramMap = new HashMap<String, DoubleValue>();
+        bigramMap = new HashMap<String, DoubleValue>();
+        unigramMap.put("<S>", new DoubleValue());
+        unigramMap.put("</S>", new DoubleValue());
+        unigramMap.put("<UNK>", new DoubleValue());
     }
 
     /** Train the model on a List of sentences represented as
      *  Lists of String tokens */
     public void train (List<List<String>> sentences) {
-		// Accumulate unigram and bigram counts in maps
-		trainSentences(sentences);
-		// Compure final unigram and bigram probs from counts
-		calculateProbs();
+        // Accumulate unigram and bigram counts in maps
+        trainSentences(sentences);
+        // Compure final unigram and bigram probs from counts
+        calculateProbs();
     }
 
     /** Accumulate unigram and bigram counts for these sentences */
     public void trainSentences (List<List<String>> sentences) {
-		for (List<String> sentence : sentences) {
-			trainSentence(sentence);
-		}
+        for (List<String> sentence : sentences) {
+            trainSentence(sentence);
+        }
     }
 
     /** Accumulate unigram and bigram counts for this sentence */
     public void trainSentence (List<String> sentence) {
-		// First count an initial start sentence token
-		String prevToken = "<S>";
-		DoubleValue unigramValue = unigramMap.get("<S>");
-		unigramValue.increment();
-		tokenCount++;
-		// For each token in sentence, accumulate a unigram and bigram count
-		for (String token : sentence) {
-			unigramValue = unigramMap.get(token);
-			// If this is the first time token is seen then count it
-			// as an unkown token (<UNK>) to handle out-of-vocabulary
-			// items in testing
-			if (unigramValue == null) {
-				// Store token in unigram map with 0 count to indicate that
-				// token has been seen but not counted
-				unigramMap.put(token, new DoubleValue());
-				token = "<UNK>";
-				unigramValue = unigramMap.get(token);
-			}
-			unigramValue.increment();    // Count unigram
-			tokenCount++;               // Count token
-			// Make bigram string
-			String bigram = bigram(prevToken, token);
-			DoubleValue bigramValue = bigramMap.get(bigram);
-			if (bigramValue == null) {
-				// If previously unseen bigram, then
-				// initialize it with a value
-				bigramValue = new DoubleValue();
-				bigramMap.put(bigram, bigramValue);
-			}
-			// Count bigram
-			bigramValue.increment();
-			prevToken = token;
-		}
-		// Account for end of sentence unigram
-		unigramValue = unigramMap.get("</S>");
-		unigramValue.increment();
-		tokenCount++;
-		// Account for end of sentence bigram
-		String bigram = bigram(prevToken, "</S>");
-		DoubleValue bigramValue = bigramMap.get(bigram);
-		if (bigramValue == null) {
-			bigramValue = new DoubleValue();
-			bigramMap.put(bigram, bigramValue);
-		}
-		bigramValue.increment();
+        // First count an initial start sentence token
+        String prevToken = "<S>";
+        DoubleValue unigramValue = unigramMap.get("<S>");
+        unigramValue.increment();
+        tokenCount++;
+        // For each token in sentence, accumulate a unigram and bigram count
+        for (String token : sentence) {
+            unigramValue = unigramMap.get(token);
+            // If this is the first time token is seen then count it
+            // as an unkown token (<UNK>) to handle out-of-vocabulary
+            // items in testing
+            if (unigramValue == null) {
+                // Store token in unigram map with 0 count to indicate that
+                // token has been seen but not counted
+                unigramMap.put(token, new DoubleValue());
+                token = "<UNK>";
+                unigramValue = unigramMap.get(token);
+            }
+            unigramValue.increment();    // Count unigram
+            tokenCount++;               // Count token
+            // Make bigram string
+            String bigram = bigram(prevToken, token);
+            DoubleValue bigramValue = bigramMap.get(bigram);
+            if (bigramValue == null) {
+                // If previously unseen bigram, then
+                // initialize it with a value
+                bigramValue = new DoubleValue();
+                bigramMap.put(bigram, bigramValue);
+            }
+            // Count bigram
+            bigramValue.increment();
+            prevToken = token;
+        }
+        // Account for end of sentence unigram
+        unigramValue = unigramMap.get("</S>");
+        unigramValue.increment();
+        tokenCount++;
+        // Account for end of sentence bigram
+        String bigram = bigram(prevToken, "</S>");
+        DoubleValue bigramValue = bigramMap.get(bigram);
+        if (bigramValue == null) {
+            bigramValue = new DoubleValue();
+            bigramMap.put(bigram, bigramValue);
+        }
+        bigramValue.increment();
     }
 
     /** Compute unigram and bigram probabilities from unigram and bigram counts */
     public void calculateProbs() {
-		// Set bigram values to conditional probability of second token given first
-		for (Map.Entry<String, DoubleValue> entry : bigramMap.entrySet()) {
-			// An entry in the HashMap maps a token to a DoubleValue
-			String bigram = entry.getKey();
-			// The value for the token is in the value of the DoubleValue
-			DoubleValue value = entry.getValue();
-			double bigramCount = value.getValue();
-			String token1 = bigramToken1(bigram); // Get first token of bigram
-			// Prob is ratio of bigram count to token1 unigram count
-			double condProb = bigramCount / unigramMap.get(token1).getValue();
-			// Set map value to conditional probability
-			value.setValue(condProb);
-		}
-		// Store unigrams with zero count to remove from map
-		List<String> zeroTokens = new ArrayList<String>();
-		// Set unigram values to unigram probability
-		for (Map.Entry<String, DoubleValue> entry : unigramMap.entrySet()) {
-			// An entry in the HashMap maps a token to a DoubleValue
-			String token = entry.getKey();
-			// Uniggram count is the current map value
-			DoubleValue value = entry.getValue();
-			double count = value.getValue();
-			if (count == 0) {
+        // Set bigram values to conditional probability of second token given first
+        for (Map.Entry<String, DoubleValue> entry : bigramMap.entrySet()) {
+            // An entry in the HashMap maps a token to a DoubleValue
+            String bigram = entry.getKey();
+            // The value for the token is in the value of the DoubleValue
+            DoubleValue value = entry.getValue();
+            double bigramCount = value.getValue();
+            String token1 = bigramToken1(bigram); // Get first token of bigram
+            // Prob is ratio of bigram count to token1 unigram count
+            double condProb = bigramCount / unigramMap.get(token1).getValue();
+            // Set map value to conditional probability
+            value.setValue(condProb);
+        }
+        // Store unigrams with zero count to remove from map
+        List<String> zeroTokens = new ArrayList<String>();
+        // Set unigram values to unigram probability
+        for (Map.Entry<String, DoubleValue> entry : unigramMap.entrySet()) {
+            // An entry in the HashMap maps a token to a DoubleValue
+            String token = entry.getKey();
+            // Uniggram count is the current map value
+            DoubleValue value = entry.getValue();
+            double count = value.getValue();
+            if (count == 0)
                 // If count is zero (due to first encounter as <UNK>)
                 // then remove save it to remove from map
                 zeroTokens.add(token);
-            } else {
+            else
                 // Set map value to prob of unigram
                 value.setValue(count / tokenCount);
-            }
-		}
-		// Remove zero count unigrams from map
-		for (String token : zeroTokens) {
-			unigramMap.remove(token);
-		}
+        }
+        // Remove zero count unigrams from map
+        for (String token : zeroTokens)
+            unigramMap.remove(token);
     }
 
     /** Return bigram string as two tokens separated by a newline */
     public String bigram (String prevToken, String token) {
-		return prevToken + "\n" + token;
+        return prevToken + "\n" + token;
     }
 
     /** Return fist token of bigram (substring before newline) */
     public String bigramToken1 (String bigram) {
-	int newlinePos = bigram.indexOf("\n");
-	return bigram.substring(0,newlinePos);
+        int newlinePos = bigram.indexOf("\n");
+        return bigram.substring(0,newlinePos);
     }
 
     /** Return second token of bigram (substring after newline) */
     public String bigramToken2 (String bigram) {
-	int newlinePos = bigram.indexOf("\n");
-	return bigram.substring(newlinePos + 1, bigram.length());
+        int newlinePos = bigram.indexOf("\n");
+        return bigram.substring(newlinePos + 1, bigram.length());
     }
 
     /** Print model as lists of unigram and bigram probabilities */
@@ -176,7 +174,7 @@ public class BigramModel {
             // The value for the token is in the value of the DoubleValue
             DoubleValue value = entry.getValue();
             System.out.println(bigramToken2(bigram) + " given " + bigramToken1(bigram) +
-                       " : " + value.getValue());
+                    " : " + value.getValue());
         }
     }
 
@@ -201,7 +199,7 @@ public class BigramModel {
         double perplexity = Math.exp(-totalLogProb / totalNumTokens);
         System.out.println("Perplexity = " + perplexity );
     }
-    
+
     /* Compute log probability of sentence given current model */
     public double sentenceLogProb (List<String> sentence) {
         // Set start-sentence as initial token
@@ -251,7 +249,7 @@ public class BigramModel {
         double perplexity = Math.exp(-totalLogProb / totalNumTokens);
         System.out.println("Word Perplexity = " + perplexity );
     }
-    
+
     /** Like sentenceLogProb but excludes predicting end-of-sentence when computing prob */
     public double sentenceLogProb2 (List<String> sentence) {
         String prevToken = "<S>";
@@ -303,7 +301,7 @@ public class BigramModel {
         return tokenProbs;
     }
 
-    /** Interpolate bigram prob using bigram and unigram model predictions */	 
+    /** Interpolate bigram prob using bigram and unigram model predictions */
     public double interpolatedProb(DoubleValue unigramVal, DoubleValue bigramVal) {
         double bigramProb = 0;
         // In bigram unknown then its prob is zero
@@ -322,8 +320,8 @@ public class BigramModel {
     }
 
     /** Train and test a bigram model.
-     *  Command format: "nlp.lm.BigramModel [DIR]* [TestFrac]" where DIR 
-     *  is the name of a file or directory whose LDC POS Tagged files should be 
+     *  Command format: "nlp.lm.BigramModel [DIR]* [TestFrac]" where DIR
+     *  is the name of a file or directory whose LDC POS Tagged files should be
      *  used for input data; and TestFrac is the fraction of the sentences
      *  in this data that should be used for testing, the rest for training.
      *  0 < TestFrac < 1
@@ -331,36 +329,41 @@ public class BigramModel {
      *  for training.
      */
     public static void main(String[] args) throws IOException {
-		// All but last arg is a file/directory of LDC tagged input data
-		File[] files = new File[args.length - 1];
-		for (int i = 0; i < files.length; i++)
-			files[i] = new File(args[i]);
-		// Last arg is the TestFrac
-		double testFraction = Double.valueOf(args[args.length -1]);
-		// Get list of sentences from the LDC POS tagged input files
-		List<List<String>> sentences = 	POSTaggedFile.convertToTokenLists(files);
-		int numSentences = sentences.size();
-		// Compute number of test sentences based on TestFrac
-		int numTest = (int)Math.round(numSentences * testFraction);
-		// Take test sentences from end of data
-		List<List<String>> testSentences = sentences.subList(numSentences - numTest, numSentences);
-		// Take training sentences from start of data
-		List<List<String>> trainSentences = sentences.subList(0, numSentences - numTest);
-		System.out.println("# Train Sentences = " + trainSentences.size() +
-				   " (# words = " + wordCount(trainSentences) +
-				   ") \n# Test Sentences = " + testSentences.size() +
-				   " (# words = " + wordCount(testSentences) + ")");
-		// Create a bigram model and train it.
-		BigramModel model = new BigramModel();
-		System.out.println("Training...");
-		model.train(trainSentences);
-		// Test on training data using test and test2
-		model.test(trainSentences);
-		model.test2(trainSentences);
-		System.out.println("Testing...");
-		// Test on test data using test and test2
-		model.test(testSentences);
-		model.test2(testSentences);
+        // All but last arg is a file/directory of LDC tagged input data
+        File[] files = new File[args.length - 1];
+        for (int i = 0; i < files.length; i++)
+            files[i] = new File(args[i]);
+        // Last arg is the TestFrac
+        double testFraction = Double.valueOf(args[args.length -1]);
+        // Get list of sentences from the LDC POS tagged input files
+        List<List<String>> sentences = 	POSTaggedFile.convertToTokenLists(files);
+        int numSentences = sentences.size();
+        System.out.println("numSentences: " + numSentences);
+        // Compute number of test sentences based on TestFrac
+        int numTest = (int) Math.round(numSentences * testFraction);
+        System.out.println("numTest: " + numTest);
+        // Take test sentences from end of data
+        List<List<String>> testSentences = sentences.subList(numSentences - numTest, numSentences);
+        // Take training sentences from start of data
+        List<List<String>> trainSentences = sentences.subList(0, numSentences - numTest);
+//        System.out.println("sentences: " + sentences);
+//        System.out.println("trainSentences: " + trainSentences);
+//        System.out.println("testSentences: " + testSentences);
+        System.out.println("# Train Sentences = " + trainSentences.size() +
+                " (# words = " + wordCount(trainSentences) +
+                ") \n# Test Sentences = " + testSentences.size() +
+                " (# words = " + wordCount(testSentences) + ")");
+        // Create a bigram model and train it.
+        BigramModel model = new BigramModel();
+        System.out.println("Training...");
+        model.train(trainSentences);
+        // Test on training data using test and test2
+        model.test(trainSentences);
+        model.test2(trainSentences);
+        System.out.println("Testing...");
+        // Test on test data using test and test2
+        model.test(testSentences);
+        model.test2(testSentences);
     }
 
 }
